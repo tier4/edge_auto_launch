9885cd3a63d621450d8bd209626d7edca392aded
David Wong <david.wong@tier4.jp>
2023-09-29 15:48:44 +0900

    chore: add copyright and newlines to pass ci
    Signed-off-by: David Wong <david.wong@tier4.jp>

--- edge_auto_launch/launch/component/lidar_camera_tf_publisher.launch.py
+++ edge_auto_launch/launch/component/lidar_camera_tf_publisher.launch.py
@@ -1,9 +1,27 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Tier IV, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import json
+
 
 def launch_setup(context, *args, **kwargs):
     tf_file = LaunchConfiguration('tf_file_path').perform(context)
@@ -44,6 +62,7 @@ def launch_setup(context, *args, **kwargs):
             ])
     ]
 
+
 def generate_launch_description():
     return LaunchDescription(
         [
@@ -51,4 +70,5 @@ def generate_launch_description():
             OpaqueFunction(function=launch_setup),
         ]
     )
+
 
