72fdb24410110e9bedc31e9ecaeedf56e8fdc56b
amc-nu <abraham.monrroy@gmail.com>
2024-09-05 09:21:24 +0900

    launch/component. fixes pep257, E302 and W391
    Signed-off-by: amc-nu <abraham.monrroy@gmail.com>

--- edge_auto_launch/launch/component/lidar_camera_tf_publisher.launch.py
+++ edge_auto_launch/launch/component/lidar_camera_tf_publisher.launch.py
@@ -1,3 +1,19 @@
+# -=- encoding: utf-8 -=-
+#
+# Copyright (C) 2024 TIER IV. All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import OpaqueFunction
@@ -5,7 +21,9 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import json
 
+
 def launch_setup(context, *args, **kwargs):
+    """Returns Launch Configuration for the TF between the lidar and the camera"""
     tf_file = LaunchConfiguration('tf_file_path').perform(context)
     with open(tf_file, 'r') as f:
         tf_data = json.load(f)
@@ -44,6 +62,7 @@ def launch_setup(context, *args, **kwargs):
             ])
     ]
 
+
 def generate_launch_description():
     return LaunchDescription(
         [
@@ -51,4 +70,3 @@ def generate_launch_description():
             OpaqueFunction(function=launch_setup),
         ]
     )
-
